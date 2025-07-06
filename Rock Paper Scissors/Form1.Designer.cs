8f1f47a84ad06db5ec94757af81cfb6f1a9db53c
tstantonECS <ts2g20@soton.ac.uk>
2025-07-06 02:41:23 +0100

    Add project files.

---
+++ Rock Paper Scissors/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Rock_Paper_Scissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paperButton = new System.Windows.Forms.Button();
+            this.scissorsButton = new System.Windows.Forms.Button();
+            this.rockButton = new System.Windows.Forms.Button();
+            this.spockButton = new System.Windows.Forms.Button();
+            this.lizardButton = new System.Windows.Forms.Button();
+            this.playerPicture = new System.Windows.Forms.PictureBox();
+            this.computerPicture = new System.Windows.Forms.PictureBox();
+            this.playerLabel = new System.Windows.Forms.Label();
+            this.computerLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.playerPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paperButton
+            // 
+            this.paperButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paperButton.Location = new System.Drawing.Point(201, 355);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(98, 59);
+            this.paperButton.TabIndex = 0;
+            this.paperButton.Tag = "1";
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = true;
+            this.paperButton.Click += new System.EventHandler(this.buttonPressed);
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scissorsButton.Location = new System.Drawing.Point(314, 355);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(98, 59);
+            this.scissorsButton.TabIndex = 1;
+            this.scissorsButton.Tag = "2";
+            this.scissorsButton.Text = "Scissors";
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            this.scissorsButton.Click += new System.EventHandler(this.buttonPressed);
+            // 
+            // rockButton
+            // 
+            this.rockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rockButton.Location = new System.Drawing.Point(89, 355);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(98, 59);
+            this.rockButton.TabIndex = 2;
+            this.rockButton.Tag = "0";
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.buttonPressed);
+            // 
+            // spockButton
+            // 
+            this.spockButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spockButton.Location = new System.Drawing.Point(543, 355);
+            this.spockButton.Name = "spockButton";
+            this.spockButton.Size = new System.Drawing.Size(98, 59);
+            this.spockButton.TabIndex = 3;
+            this.spockButton.Tag = "4";
+            this.spockButton.Text = "Spock";
+            this.spockButton.UseVisualStyleBackColor = true;
+            this.spockButton.Click += new System.EventHandler(this.buttonPressed);
+            // 
+            // lizardButton
+            // 
+            this.lizardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lizardButton.Location = new System.Drawing.Point(427, 355);
+            this.lizardButton.Name = "lizardButton";
+            this.lizardButton.Size = new System.Drawing.Size(98, 59);
+            this.lizardButton.TabIndex = 4;
+            this.lizardButton.Tag = "3";
+            this.lizardButton.Text = "Lizard";
+            this.lizardButton.UseVisualStyleBackColor = true;
+            this.lizardButton.Click += new System.EventHandler(this.buttonPressed);
+            // 
+            // playerPicture
+            // 
+            this.playerPicture.Location = new System.Drawing.Point(145, 201);
+            this.playerPicture.Name = "playerPicture";
+            this.playerPicture.Size = new System.Drawing.Size(100, 102);
+            this.playerPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.playerPicture.TabIndex = 5;
+            this.playerPicture.TabStop = false;
+            // 
+            // computerPicture
+            // 
+            this.computerPicture.Location = new System.Drawing.Point(480, 201);
+            this.computerPicture.Name = "computerPicture";
+            this.computerPicture.Size = new System.Drawing.Size(100, 102);
+            this.computerPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.computerPicture.TabIndex = 6;
+            this.computerPicture.TabStop = false;
+            // 
+            // playerLabel
+            // 
+            this.playerLabel.AutoSize = true;
+            this.playerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerLabel.Location = new System.Drawing.Point(141, 174);
+            this.playerLabel.Name = "playerLabel";
+            this.playerLabel.Size = new System.Drawing.Size(105, 16);
+            this.playerLabel.TabIndex = 7;
+            this.playerLabel.Text = "Player Choice";
+            // 
+            // computerLabel
+            // 
+            this.computerLabel.AutoSize = true;
+            this.computerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computerLabel.Location = new System.Drawing.Point(467, 174);
+            this.computerLabel.Name = "computerLabel";
+            this.computerLabel.Size = new System.Drawing.Size(126, 16);
+            this.computerLabel.TabIndex = 8;
+            this.computerLabel.Text = "Computer Choice";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(228, 101);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(288, 16);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.Text = "Make your choice from the buttons below";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox.Location = new System.Drawing.Point(43, 48);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(221, 20);
+            this.checkBox.TabIndex = 10;
+            this.checkBox.Text = "Computer Copies Last Move";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.computerLabel);
+            this.Controls.Add(this.playerLabel);
+            this.Controls.Add(this.computerPicture);
+            this.Controls.Add(this.playerPicture);
+            this.Controls.Add(this.lizardButton);
+            this.Controls.Add(this.spockButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.scissorsButton);
+            this.Controls.Add(this.paperButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.playerPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button scissorsButton;
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button spockButton;
+        private System.Windows.Forms.Button lizardButton;
+        private System.Windows.Forms.PictureBox playerPicture;
+        private System.Windows.Forms.PictureBox computerPicture;
+        private System.Windows.Forms.Label playerLabel;
+        private System.Windows.Forms.Label computerLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.CheckBox checkBox;
+    }
+}
+
---
+++ Rock Paper Scissors/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace Rock_Paper_Scissors
+{
+    public partial class Form1 : Form
+    {
+        bool copyLastMove = false;                                                                                  //Create boolean variable to determine whether the computer should copy the player's last move.
+        int? lastPlayerMove;                                                                                        //Create an nullable integer variable to store the player's last move.
+        Random random = new Random();                                                                               //Create an instance of the Random class to be used for random number generation.
+        int playerChoice, computerChoice;                                                                           //Create variables to store the player and computer's choices in integer form. 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonPressed(object sender, EventArgs e)                                                      //Function is called whenever one of the player choice buttons is pressed.
+        {
+            Button tempButton = sender as Button;                                                                   //Get the button that was pressed.
+            playerChoice = Convert.ToInt32(tempButton.Tag);                                                         //Convert that button's tag into an integer (each button has a unique integer tag).
+            if (copyLastMove && lastPlayerMove != null)
+            {                
+                computerChoice = (int)lastPlayerMove;                                                               //If the computer is set to copy the player's last move and the player has already made a move, the computer will copy the player's last move.
+            }
+            else
+            {
+                computerChoice = generateChoice();                                                                  //If the computer is not set to copy the player's last move or the player has not already made a move, Generate a random number from 0 to 4 to be the computer's choice.
+            }
+            
+            playerLabel.Text = "Player chooses " + changeLabelandImage(playerChoice, playerPicture);                //Update the player choice label to display their choice and update the player's picture box to show an image of their choice.
+            computerLabel.Text = "Computer chooses " + changeLabelandImage(computerChoice, computerPicture);        //Update the computer choice label to display the computer's choice and update the computer's picture box to show an image of its choice.
+            checkGame();                                                                                            //Call the CheckGame function to decide who wins.
+            lastPlayerMove = playerChoice;                                                                          //After the game has been decided, set the last players move to be the current player's choice.
+        }
+
+        private int generateChoice()
+        {
+            return random.Next(0, 5);
+        }
+        private string changeLabelandImage(int choice, PictureBox choiceBox)
+        {
+            string choiceString = null;                                                                             //Create a word variable of type string to store the string each choice, starting with a null value.
+            switch (choice)                                                                                         //Start a switch statement to return the correct string and change the image of the picture box to the correct image.
+            {
+                case 0:                                                                                             //If the choice is 0, return "Rock" and display the rock image.    
+                    choiceString = "Rock";      
+                    choiceBox.Image = Properties.Resources.rock;
+                    break;
+                case 1:                                                                                             //If the choice is 1, return "Paper" and display the paper image.
+                    choiceString = "Paper";
+                    choiceBox.Image = Properties.Resources.paper;
+                    break;
+                case 2:                                                                                             //If the choice is 2, return "Scissors" and display the scissors image.
+                    choiceString = "Scissors";
+                    choiceBox.Image = Properties.Resources.scissors;
+                    break;
+                case 3:                                                                                             //If the choice is 3, return "Lizard" and display the lizard image.
+                    choiceString = "Lizard";
+                    choiceBox.Image = Properties.Resources.lizard;
+                    break;
+                case 4:                                                                                             //If the choice is 4, return "Spock" and display the spock image.
+                    choiceString = "Spock";
+                    choiceBox.Image = Properties.Resources.spock;
+                    break;
+            }
+            return choiceString;
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            copyLastMove = checkBox.Checked;
+        }
+
+        private void checkGame()
+        {
+            if (computerChoice == playerChoice)                                                                     //If both the player and the computer make the same choice, declare the game a draw.
+            {
+                resultLabel.Text = "It's a Draw!";
+            }
+            else if (playerChoice == 2 && computerChoice == 1 ||                                                    //If the player picks a choice that defeats the computer's choice, the player wins.
+                     playerChoice == 1 && computerChoice == 0 || 
+                     playerChoice == 0 && computerChoice == 3 ||
+                     playerChoice == 3 && computerChoice == 4 ||
+                     playerChoice == 4 && computerChoice == 2 ||
+                     playerChoice == 2 && computerChoice == 3 ||
+                     playerChoice == 3 && computerChoice == 1 ||
+                     playerChoice == 1 && computerChoice == 4 ||
+                     playerChoice == 4 && computerChoice == 0 ||
+                     playerChoice == 0 && computerChoice == 2
+                     )
+            {
+                resultLabel.Text = "Player wins!";
+            }
+            else                                                                                                    //Otherwise, the computer wins.
+            {
+                resultLabel.Text = "Computer wins!";
+            }
+        }
+    }
+}
